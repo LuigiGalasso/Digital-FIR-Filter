6e5ed4276a53de54d5c07c579bd69526b1945048
Luigi Galasso <60068789+luigi19@users.noreply.github.com>
2021-12-13 11:32:08 +0100

    Delete syn directory

--- syn/simulation/constants.vhd
+++
@@ -1,6 +0,0 @@
-package CONSTANTS is
-   constant NB : integer := 14;
-   constant NT : integer := 9;
-   --subtype coeff is array (0 to NT - 1) of std_logic_vector(NB - 1 downto 0);
-
-end CONSTANTS;
